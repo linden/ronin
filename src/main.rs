9624b1a001a8c1d0e90b5af0a3fc826b1c105335
asteria <asteria@syvita.org>
2021-12-24 13:23:53 +0000

    dababy

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[cfg(test)] mod tests;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![hello])
+        .launch();
+}
